e607dbb2c0bd038afc76e3bf08e265426ac45d45
Fabiano <fabian.skrzy@gmail.com>
2026-05-22 21:59:02 +0200

    Event model created

--- MauiApp1/Models/CalendarEvent.cs
+++ MauiApp1/Models/CalendarEvent.cs
@@ -1,6 +1,14 @@
 ﻿namespace MauiApp1.Models;
 
-public class CalendarEvent
+public class Event
 {
-    
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string User { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public String Color { get; set; }
+    public bool IsHighPriority { get; set; }
+
 }
