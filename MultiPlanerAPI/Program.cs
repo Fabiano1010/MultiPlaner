f16e8528776f02cbdc99f22c8af31434b2556385
Barsol6 <bartosz.solyga@gmail.com>
2026-08-16 14:24:15 +0200

    feat: added database migration

--- MultiPlanerAPI/Program.cs
+++ MultiPlanerAPI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MultiPlanerAPI.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -8,11 +9,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-                       ?? throw new InvalidOperationException("Not found connection string 'DefaultConnection'.");
+    ?? throw new InvalidOperationException("Not found 'DefaultConnection' in appsettings.json.");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
+    options.UseSqlServer(connectionString, sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(5),
+            errorNumbersToAdd: null);
+    }));
 
 
 builder.Services.AddCors(options =>
@@ -20,12 +26,33 @@ builder.Services.AddCors(options =>
     options.AddPolicy("DevPolicy", policy =>
     {
         policy.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var logger = services.GetRequiredService<ILogger<Program>>();
+
+    try
+    {
+        var db = services.GetRequiredService<AppDbContext>();
+        logger.LogInformation("DB migration started...");
+        
+        db.Database.Migrate();
+        
+        logger.LogInformation("Migrations succesfully aplied");
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "Migrations failed");
+        throw; 
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -35,13 +62,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("DevPolicy");
 app.UseHttpsRedirection();
-app.MapControllers();
 app.UseAuthorization();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
+app.MapControllers();
 
 app.Run();
