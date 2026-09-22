bda2fda7d0015b3f665c4530ed0648224168d978
Fabiano1010 <fabian.skrzy@gmail.com>
2026-09-22 13:33:45 +0200

    feat: add locale.js for timezone and country code detection, update Register component to utilize new localization functions

--- MultiPlanerWeb/Program.cs
+++ MultiPlanerWeb/Program.cs
@@ -1,5 +1,7 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using MultiPlanerWeb;
 using MultiPlanerSharedModels.Services;
 using MultiPlanerSharedModels.Models;
@@ -30,4 +32,23 @@ builder.Services.AddScoped<AuthService>();
 // Domain Services
 builder.Services.AddScoped<EventService>();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+// Set culture based on browser settings
+try
+{
+    var js = host.Services.GetRequiredService<IJSInProcessRuntime>();
+    var browserCulture = js.Invoke<string>("eval", "navigator.language");
+    if (!string.IsNullOrWhiteSpace(browserCulture))
+    {
+        var culture = new CultureInfo(browserCulture);
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+    }
+}
+catch
+{
+    // default culture will be used
+}
+
+await host.RunAsync();
